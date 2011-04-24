790234d52ab852456ea7b2f37ecddd1954109b4a
maurycy <maurycy@g.pl>
2011-04-24 05:42:05 +0200

    Simplify even further.

--- lib/meta_meta/chain.rb
+++ lib/meta_meta/chain.rb
@@ -1,120 +1,86 @@
 module MetaMeta
   class Chain
-    attr_accessor :archive, :base, :queue, :stored, :referenced, :aliased
-    
-    def i_suck(foo=nil)
-      # XXX: self.foo= v. @foo= ???
-      self.base = foo unless foo.nil?
-      self.queue = {}
-      self.stored = {}
-      self.referenced = {}
-      self.aliased = []
-    end
+    attr_accessor :base
+    attr_accessor :referenced, :stored, :queue
     
     def initialize(base, &blk)
-      i_suck(base)
-
-      # Set up within a block.
-      instance_eval(&blk) if block_given?
-    end
-    
-    # Update the queue. Then, do nothing if method undefined. Otherwise, if
-    # first time, rename it and alias with my_method.
-    
-    def before(name, *args)
-      return self unless base.method_defined?(name)
-      
-      (queue[name] ||= [nil]).insert(0, *args).flatten!
-      
-      return self if stored.has_key?(name)
-
-      referenced[name] = store(name)
-
-      base.send(:define_method, name) do |*args, &blk|
-        self.class.chain.send(:my_method, self, name, *args, &blk)
-      end
-      
-      self
+      self.base = base
+      self.initialize!
+      self.instance_eval(&blk) if block_given?
     end
 
     def after(name, *args)
-      return self unless base.method_defined?(name)
-      
-      (queue[name] ||= [nil]).push(*args).flatten!
-      
-      return self if stored.has_key?(name)
-
-      referenced[name] = store(name)
-
-      base.send(:define_method, name) do |*args, &blk|
-        self.class.chain.send(:my_method, self, name, *args, &blk)
+      act!(:my_method, name) do |q, random_name|
+        referenced[name] = random_name
+        q.insert(-1, *args).flatten!
       end
-      
-      self
+    end
+    
+    def before(name, *args)
+      act!(:my_method, name) do |q, random_name|
+        referenced[name] = random_name
+        q.insert(0, *args).flatten!
+      end
     end
 
-
     def replace(name, replace)
-      return self unless base.method_defined?(name)
-      return self     if stored.has_key?(name)
-      
-      queue[name] ||= [nil]
-      referenced[name] = replace
-      
-      base.send(:define_method, name) do |*args, &blk|
-        self.class.chain.send(:my_method, self, name, *args, &blk)
-      end
-      
-      self
+      act!(:my_method, name) {|q, random_name| referenced[name] = replace}
     end
     
     def remove(*args)
-      args.flatten!
-      name = args.shift
-      args.each {|arg| remove(arg)} if args.any?
-
-      referenced[name] = nil
-      return self     if stored.has_key?(name)
-      return self unless base.method_defined?(name)
-      store(name)
-      
-      base.send(:remove_method, name)
-      self
+      args.flatten.each {|name| act!(nil, name) {|b,c|}}
     end
     
     def flush!
       stored.each do |name, random_name|
         base.method_defined?(random_name) || fail('BUG')
-        base.method_defined?(name) && base.send(:remove_method, name)
+        base.method_defined?(name)        && base.send(:remove_method, name)
         base.class_eval { alias_method(name, random_name) }
       end
 
-      i_suck
+      initialize!
       self
     end
     
     protected
-      def store(name)
-        stored.has_key?(name) && fail('BUG')
-        keep(name).tap {|random_name| stored[name] = random_name }
+      def initialize!
+        self.referenced, self.stored, self.queue = {}, {}, {}
+      end
+      
+      def act!(what, name)
+        base.method_defined?(name) || (return(self))
+        
+        queue[name] ||= [queue[name]]
+        queue[name].tap {|q| yield(q, store_method(name)) }
+        
+        case what
+        when nil
+          base.send(:remove_method, name)
+        else
+          base.send(:define_method, name) do |*args, &blk|
+            self.class.chain.send(:my_method, self, name, *args, &blk)
+          end
+        end
+
+        self
+      end
+    
+      def store_method(name)
+        stored[name] ||= minify_method(name)
       end
 
-      # Hide method with a random unique name, and return the name. Raise an
-      # exception if method undefined.
-      #--
-      # FIXME: random makes uneasy read
-      # FIXME: it cannot include numbers
-      #++
-      def keep(name)
+      def minify_method(name)
         ('a' + rand(36**16).to_s(36)).to_sym.tap do |random_name|
-          base.method_defined?(random_name) && fail('BUG')
+          base.method_defined?(random_name) && fail('XXX')
           base.class_eval { alias_method(random_name, name) }
         end
       end
       
+      #--
       # XXX: Kernel#__method__ broken
       # XXX: must be fast
       # XXX: super goes to the method, not #my_method?
+      #++
       def my_method(instance, name, *args, &blk)
         ref, ret = referenced[name], nil
 
